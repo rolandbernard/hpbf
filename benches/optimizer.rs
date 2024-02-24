c7ee71f83121698bbe187b0f32cd5bbc90a7e422
Roland Bernard <rolli.bernard@gmail.com>
2024-02-24 16:32:12 +0100

    Added additional benches.

--- benches/optimizer.rs
+++ benches/optimizer.rs
@@ -4,8 +4,16 @@ use hpbf::{CellType, Program};
 use iai_callgrind::{library_benchmark, library_benchmark_group, main};
 use std::{fs, hint::black_box};
 
+fn read_file(file: &str) -> String {
+    fs::read_to_string(file).unwrap()
+}
+
+fn parse_program(code: &str) -> Program<u8> {
+    Program::parse(code).unwrap()
+}
+
 fn read_and_parse(file: &str) -> Program<u8> {
-    Program::parse(&fs::read_to_string(file).unwrap()).unwrap()
+    parse_program(&read_file(file))
 }
 
 fn optimize<C: CellType>(program: Program<C>, level: u32) -> Program<C> {
@@ -20,14 +28,41 @@ fn optimize<C: CellType>(program: Program<C>, level: u32) -> Program<C> {
     "examples/fib.bf",
     "examples/primes.bf",
     "examples/mandel.bf",
+    "examples/dbfi.bf",
+    "examples/bsort.bf",
+    "examples/qsort.bf",
+    "examples/factor.bf",
+    "examples/factorial.bf",
+    "examples/factorial2.bf",
+    "examples/widthcheck.bf",
+], setup = read_file)]
+fn bench_parse(code: String) -> Program<u8> {
+    black_box(parse_program(&code))
+}
+
+#[library_benchmark]
+#[benches::with_setup(args = [
+    "examples/hello.bf",
+    "examples/hello2.bf",
+    "examples/hanoi.bf",
+    "examples/fib.bf",
+    "examples/primes.bf",
+    "examples/mandel.bf",
+    "examples/dbfi.bf",
+    "examples/bsort.bf",
+    "examples/qsort.bf",
+    "examples/factor.bf",
+    "examples/factorial.bf",
+    "examples/factorial2.bf",
+    "examples/widthcheck.bf",
 ], setup = read_and_parse)]
-fn bench_opt_level_default(program: Program<u8>) -> Program<u8> {
-    black_box(optimize(program, 1))
+fn bench_optimize(program: Program<u8>) -> Program<u8> {
+    black_box(optimize(program, 2))
 }
 
 library_benchmark_group!(
-    name = bench_opt_group;
-    benchmarks = bench_opt_level_default
+    name = bench_group;
+    benchmarks = bench_optimize, bench_parse
 );
 
-main!(library_benchmark_groups = bench_opt_group);
+main!(library_benchmark_groups = bench_group);
